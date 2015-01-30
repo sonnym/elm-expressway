8e5e1b073f7a946ff9e3e6f7f65be10bc9bd7695
sonnym <michaud.sonny@gmail.com>
2015-01-29 22:24:08 -0500

    automatically rebuild client output on elm changes

--- generator/gulpfile.js
+++ generator/gulpfile.js
@@ -3,3 +3,5 @@ var gulp = require("gulp");
 require("elm-expressway/gulpfile")(gulp, "<%= appName %>/Client.elm");
 
 gulp.task("default", ["elm-expressway_default"]);
+
+gulp.watch("<%= appName %>/**/*.*", ["elm-expressway_compile"]);
